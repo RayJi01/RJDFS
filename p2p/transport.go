21d0a0a4ebc11d6279c56c3ce75493453f30d74e
RayJi01 <ruiji@usc.edu>
2024-11-07 14:05:29 -0800

    Feature: Add Started code for p2p

--- p2p/transport.go
+++ p2p/transport.go
@@ -1,2 +1,8 @@
 package p2p
 
+// Peer: Interface that represents the remote node of the connection
+type Peer interface {}
+
+// Tranasport: Interface that represents any handles between connections
+// the connection can either be TCP, UDP, Grpc, ...
+type Transport interface {}
